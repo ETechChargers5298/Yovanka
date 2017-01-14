66d580c25246a57922ad50d23dd1ba8b1afede19
ETechChargers5298 <frc5298@gmail.com>
2017-01-14 13:02:47 -0500

    Code for basic movement (incomplete)

---
+++ src/org/usfirst/frc/team5298/robot/commands/DriveTrainCommands.java
@@ -0,0 +1,39 @@
+package org.usfirst.frc.team5298.robot.commands;
+
+import org.usfirst.frc.team5298.robot.OI;
+import org.usfirst.frc.team5298.robot.Robot;
+import org.usfirst.frc.team5298.robot.subsystems.DriveTrain;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class DriveTrainCommands extends Command {
+	
+	public DriveTrainCommands() {
+		
+	}
+	
+	   protected void initialize() {
+	    }
+
+	    // Called repeatedly when this Command is scheduled to run
+	    protected  void execute() {
+	    	double magnitude = Robot.oi.driverPad.getLeftY();
+	    	double direction = Robot.oi.driverPad.getRightY();
+	    	double rotation = Robot.oi.driverPad.getRightX();
+	    	
+	    }
+
+	    // Make this return true when this Command no longer needs to run execute()
+	    protected boolean isFinished() {
+	        return false;
+	    }
+
+	    // Called once after isFinished returns true
+	    protected void end() {
+	    }
+
+	    // Called when another command which requires one or more of the same
+	    // subsystems is scheduled to run
+	    protected void interrupted() {
+	    }
+}
